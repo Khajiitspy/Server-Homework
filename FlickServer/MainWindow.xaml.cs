f052ad7d10dfbfc7e40263fc461ea6f4c5d41d33
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-01-09 12:15:30 -0500

    Homework

---
+++ FlickServer/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlickServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetLocalIP();
+        }
+
+        public void GetLocalIP()
+        {
+            string hostName = Dns.GetHostName();
+
+            IPHostEntry localhost = Dns.GetHostEntry(hostName);
+
+            foreach (IPAddress entry in localhost.AddressList)
+            {
+                MenuItem option = new MenuItem();
+                option.Header = entry;
+                option.Click += IPOption_Click;
+
+                LocalIpMenu.Items.Add(option);
+            }
+        }
+
+        private void IPOption_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            IPInput.Text = menuItem.Header.ToString();
+        }
+
+        bool IsRunServer = false;
+
+        private void RunServer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPAddress ip = IPAddress.Parse(IPInput.Text);
+                int port = Int32.Parse(PortInput.Text);
+                Task.Run(() => RunServer(ip, port));
+                RunButton.Visibility = Visibility.Collapsed;
+                StopButton.Visibility = Visibility.Visible;
+                IsRunServer = true;
+                ServerOutput.Text += "Server Startup...";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Invalid Run data");
+            }
+        }
+
+        private async void RunServer(IPAddress IP, int port)
+        {
+            string hostName = Dns.GetHostName();
+
+            IPEndPoint iPEndPoint = new IPEndPoint(IP, port); 
+
+            Socket server = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Bind(iPEndPoint);
+                server.Listen(10);
+
+                while (IsRunServer)
+                { 
+                    Socket client = server.Accept();
+                    int bytes = 0;
+                    byte[] buffer = new byte[1024];
+                    do
+                    {
+                        bytes = client.Receive(buffer);
+                        Dispatcher.Invoke(() => { ServerOutput.Text += $"\nMessage: '{Encoding.Unicode.GetString(buffer)}'"; });
+                    } while (client.Available > 0);
+                    string message = $"\nMessage Recieved {DateTime.Now}";
+                    buffer = Encoding.Unicode.GetBytes(message);
+                    client.Send(buffer);
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Problem {ex.Message}");
+            }
+        }
+
+        private void StopServer_Click(object sender, RoutedEventArgs e)
+        {
+            IsRunServer = false;
+            RunButton.Visibility = Visibility.Visible;
+            StopButton.Visibility = Visibility.Collapsed;
+            ServerOutput.Text = "";
+        }
+
+        ~MainWindow() {
+            IsRunServer = false;
+        }
+    }
+}
---
+++ ServerConnector/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServerConnector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public async void SendMessage(IPAddress IP, int port, string Message)
+        {
+            try
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IP, port);
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                server.Connect(serverEndPoint);
+                var data = Encoding.Unicode.GetBytes(Message);
+                server.Send(data);
+
+                data = new byte[1024];
+                int bytes = 0;
+                do
+                {
+                    bytes = server.Receive(data);
+                    MessageBox.Show($"Server answered {Encoding.Unicode.GetString(data)}");
+
+                } while (server.Available > 0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SendMessage(IPAddress.Parse(IPInput.Text), Int32.Parse(PortInput.Text), UserMessageInput.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Invalid Data!");
+            }
+        }
+    }
+}
